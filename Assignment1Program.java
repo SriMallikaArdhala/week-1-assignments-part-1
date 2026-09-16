098622980816630a62538bfdfa5413a6405d9f87
SriMallikaArdhala <ardhalamallika@gamail.com>
2026-09-16 20:09:54 +0530

    second program is completed.

--- Assignment1Program.java
+++ Assignment1Program.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Assignment1Program {
+import static java.lang.System.out;
+import java.util.Scanner;
+public class Assignment1Program {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        out.println("Enter number:");
+        var num=sc.nextInt();
+        var status1 = num > 0;
+        var status2= num<0 ;
+        var remainder=num%2;
+        var outputtext="";
+        if(status1 || status2){
+            outputtext=status1?"it a positive number ":"it is a negative number ";
+            if(remainder==0){
+                outputtext+="and even number";
+
+            }else{
+                outputtext+="and odd number";
+            }
+            out.println(outputtext);
+        }
+        if(num==0){
+            out.println("the given number is zero");
+        }
+    }
 }
+
--- Assignment2Program.java
+++ Assignment2Program.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Assignment2Program {
+import static java.lang.System.out;
+import java.util.Scanner;
+public class Assignment2Program {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        out.println("Enter three numbers:");
+        var num1=sc.nextInt();
+        var num2=sc.nextInt();
+        var num3=sc.nextInt();
+        var largestOfTwo= (num1>num2) ? num1 : num2;
+        var largestOfThree= (largestOfTwo>num3) ? largestOfTwo : num3;
+        out.println(largestOfThree + " is the largest number.");
+
+    }
 }
